cd128c67df366bff45979afb401fd2a47b054783
DKoezle <daniekoezle@gmx.de>
2020-07-09 12:30:44 +0200

    Position des Result-Titels korrigiert

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
     path('recipe/new/', views.recipe_new, name='recipe_new'),
     path('recipe/<int:pk>/edit/', views.recipe_edit, name='recipe_edit'),
-    path('recipe/filter/cuis/<str:cuis>/', views.recipe_cuisfilter, name='recipe_cuisfilter'),
-    path('recipe/filter/aller/<str:aller>/', views.recipe_allerfilter, name='recipe_allerfilter'),
+    path('recipe/filter/cuis=<str:cuis>/', views.recipe_cuisfilter, name='recipe_cuisfilter'),
+    path('recipe/filter/aller=<str:aller>/', views.recipe_allerfilter, name='recipe_allerfilter'),
     path('recipe/advsearch', views.advanced_search, name='advanced_search'),
 ]
